c00a50987dc1988a35152a7eb6e34af83c5c5b7d
Oliver <115661717+OliKirk@users.noreply.github.com>
2023-11-29 23:07:05 +0100

    Authors og Works kommer frem

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,106 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+
+// function App() {
+
+//   const [backendData, setBackendData] = useState([{}])
+  
+//   useEffect(() => {
+//     fetch("/api").then(
+//       response => response.json()
+//     ).then(
+//       data => {
+//        setBackendData(data);
+//      }
+//    )
+//   }, [])
+
+//   return (
+//     <div>
+
+//       {(typeof backendData.users === 'undefined') ? (
+//         <p>Loading...</p>
+//       ):(
+//         backendData.users.map((user, index) => (
+//           <p key={index}>{user}</p>
+//         ))
+//       )}
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [authors, setAuthors] = useState([]);
+  const [works, setWorks] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [contactMe, setContactMe] = useState([]);
+  const [news, setNews] = useState([]);
+  const [socialMedia, setSocialMedia] = useState([]);
 
-  const [backendData, setBackendData] = useState([{}])
-  
   useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-       setBackendData(data);
-     }
-   )
-  }, [])
+    const fetchData = async () => {
+      try {
+        // Fetch data for authors
+        const authorsResponse = await axios.get('/api/authors');
+        setAuthors(authorsResponse.data);
+
+        // Fetch data for works
+        const worksResponse = await axios.get('/api/works');
+        setWorks(worksResponse.data);
+
+        // Fetch data for reviews
+        const reviewsResponse = await axios.get('/api/reviews');
+        setReviews(reviewsResponse.data);
+
+        // Fetch data for contactMe
+        const contactMeResponse = await axios.get('/api/contactMe');
+        setContactMe(contactMeResponse.data);
+
+        // Fetch data for news
+        const newsResponse = await axios.get('/api/news');
+        setNews(newsResponse.data);
+
+        // Fetch data for socialMedia
+        const socialMediaResponse = await axios.get('/api/socialMedia');
+        setSocialMedia(socialMediaResponse.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle errors if necessary
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
+      {/* Display authors */}
+      <h2>Authors:</h2>
+      <ul>
+        {authors.map((author, index) => (
+          <li key={index}>{author.name}</li>
+        ))}
+      </ul>
 
-      {(typeof backendData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ):(
-        backendData.users.map((user, index) => (
-          <p key={index}>{user}</p>
-        ))
-      )}
+      {/* Display works */}
+      <h2>Works:</h2>
+      <ul>
+        {works.map((work, index) => (
+          <li key={index}>{work.title}</li>
+        ))}
+      </ul>
+
+      {/* Display reviews, contactMe, news, socialMedia similarly */}
+      {/* ... */}
     </div>
-  )
+  );
 }
 
 export default App;
+
+
--- server/server.js
+++ server/server.js
@@ -1,10 +1,38 @@
-const express   = require('express');
-const app       = express();   
+// const express   = require('express');
+// const app       = express();   
 
-app.get('/', (require, response) => {
-  response.json(({"users": [{"name": "John", "age": 22}, {"name": "Peter", "age": 33}]}))
+// app.get('/', (require, response) => {
+//   response.json(({"users": [{"name": "John", "age": 22}, {"name": "Peter", "age": 33}]}))
+// });
+
+// app.listen(5000, () => {
+//   console.log('Example app listening on port 3000!');
+// });
+
+_______________________________________________________________________________________________________________
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+// Define a route to fetch data from the backend
+app.get('/', async (req, res) => {
+  try {
+    // Make a GET request to your backend API
+    const backendResponse = await axios.get('https://semesterprojekt2-deployment-with-azure.azurewebsites.net/');
+
+    // Assuming the response data structure is similar to your JSON example
+    const usersData = backendResponse.data.users;
+
+    // Send the fetched data as a response
+    res.json({ users: usersData });
+  } catch (error) {
+    // Handle errors if the request to the backend fails
+    res.status(500).json({ error: 'Failed to fetch data from the backend' });
+  }
 });
 
-app.listen(5000, () => {
-  console.log('Example app listening on port 3000!');
+// Start the server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Express app listening on port ${PORT}`);
 });
