41c6925e18326e21a3c539c7008dc6220a13ca9f
LauritsSchou <laurits.schou@gmail.com>
2023-12-05 16:34:57 +0100

    added bootstrap

--- src/pages/WorksPage.jsx
+++ src/pages/WorksPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function WorksPage() {
   const [works, setWorks] = useState([]);
@@ -19,24 +20,18 @@ export default function WorksPage() {
   console.log("Works page");
 
   return (
-    <div>
-      <h1>Works Page</h1>
-      <ul>
+    <div className="container mt-5">
+      <h1>Værker</h1>
+      <ul className="list-group">
         {works.map((work) => (
-          <li key={work.id}>
-            <li>
-              <p>Titel: {work.title}</p>
-            </li>
-            <li>
-              <p>Udgivelses dato: {work.publication_date}</p>
-            </li>
-            <li>
-              <p>Forlag: {work.publisher}</p>
-            </li>
-            <li>
-              <p>Beskrivelse: {work.description}</p>
-            </li>
-            {work.image}
+          <li key={work.id} className="list-group-item">
+            <p className="mb-1 h4">{work.title}</p>
+            {work.image && (
+              <img src={work.image} alt="Work Image" className="img-fluid" />
+            )}
+            <p className="mb-1">Udgivelses dato: {work.publication_date}</p>
+            <p className="mb-1">Forlag: {work.publisher}</p>
+            <p className="mb-1">Beskrivelse: {work.description}</p>
           </li>
         ))}
       </ul>
