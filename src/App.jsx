0d6632cf998d02918df6160e3febb56d7d9b0587
LauritsSchou <laurits.schou@gmail.com>
2023-12-05 14:19:16 +0100

    removed duplicate routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,6 @@ import WorksPage from "./pages/WorksPage";
 export default function App() {
   const publicRoutes = (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/works" element={<WorksPage />} />
-      <Route path="/works" element={<WorksPage />} />
-      <Route path="/works" element={<WorksPage />} />
       <Route path="/works" element={<WorksPage />} />
     </Routes>
   );
