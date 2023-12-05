ac905839d0739e9f88c2873ec4bd0b79d059e8ab
Adelkilde <William.adelkilde@gmail.com>
2023-12-05 23:43:07 +0100

    Function Naming, Parallel Requests Optimization

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -5,15 +5,16 @@ export default function AboutMePage() {
   const [author, setAuthor] = useState([]);
 
   useEffect(() => {
-    async function getAuthor() {
+    async function fetchAuthor() {
       const url = "https://semesterprojekt2-deployment-with-azure.azurewebsites.net/author";
       const response = await fetch(url);
       const data = await response.json();
       const authorArray = Object.keys(data).map((key) => ({ id: key, ...data[key] }));
       setAuthor(authorArray);
     }
-    getAuthor();
+    fetchAuthor();
   }, []);
+
   console.log("Author page");
 
   return (
@@ -24,7 +25,7 @@ export default function AboutMePage() {
           <li key={author.id} className="list-group-item">
             {author.image && <img src={author.image} alt="author Image" className="img-fluid" />}
             <p className="mb-1">{author.name}</p>
-            <p className="mb-1">f.{author.birth_year}</p>
+            <p className="mb-1">f. {author.birth_year}</p>
             <p className="mb-1">{author.biography}</p>
           </li>
         ))}
--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -4,15 +4,16 @@ export default function NewsPage() {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
-    async function getNews() {
-      const url = "http://localhost:3333/news";
+    async function fetchNews() {
+      const url = "https://semesterprojekt2-deployment-with-azure.azurewebsites.net/news";
       const response = await fetch(url);
       const data = await response.json();
       const newsArray = Object.keys(data).map((key) => ({ id: key, ...data[key] }));
       setNews(newsArray);
     }
-    getNews();
+    fetchNews();
   }, []);
+
   console.log("News page");
 
   return console.log(news);
