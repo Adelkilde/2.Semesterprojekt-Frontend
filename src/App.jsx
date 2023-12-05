3c1aad0ce9124d0296f381f64e30d13be9a990fc
Oliver Kirk <owk0907@gmail.com>
2023-12-05 14:04:57 +0100

    Start på fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router";
-import Navbar from "./components/Navbar";
-import WorksPage from "./pages/WorksPage";
+// import Navbar from "./components/Navbar";
+// import WorksPage from "./pages/WorksPage";
+import NewsPage from "./pages/NewsPage";
 
 export default function App() {
   const publicRoutes = (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/works" element={<WorksPage />} />
-      <Route path="/works" element={<WorksPage />} />
-      <Route path="/works" element={<WorksPage />} />
-      <Route path="/works" element={<WorksPage />} />
+      {/* <Route path="/" element={<HomePage />} /> */}
+      {/* <Route path="/works" element={<WorksPage />} /> */}
+      <Route path="/news" element={<NewsPage />} />
+      {/* <Route path="/works" element={<WorksPage />} /> */}
+      {/* <Route path="/works" element={<WorksPage />} /> */}
     </Routes>
   );
   return <main>{publicRoutes}</main>;
