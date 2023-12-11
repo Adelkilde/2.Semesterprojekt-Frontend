e9ea879956b4de7cb7d6753bb4749360fc2d6f12
LauritsSchou <laurits.schou@gmail.com>
2023-12-11 15:40:09 +0100

    manageReviews imported

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import News from "./pages/NewsPage.jsx";
 import ContactMe from "./pages/ContactPage.jsx";
 import ManageAboutMe from "./pages/ManageAboutMePage.jsx";
 import ManageWorks from "./pages/ManageWorksPage.jsx";
-// import ManageReviews from "./pages/ManageReviewsPage.jsx";
+import ManageReviews from "./pages/ManageReviewsPage.jsx";
 // import ManageNews from "./pages/ManageNewsPage.jsx";
 // import ManageContactMe from "./pages/ManageContactMePage.jsx";
 
@@ -40,8 +40,10 @@ export default function App() {
           {isAdmin && (
             <Route path="/admin/manage-works" element={<ManageWorks />} />
           )}
-          {/* {isAdmin && <Route path="/admin/manage-reviews" element={<ManageReviews />} />}
-          {isAdmin && <Route path="/admin/manage-news" element={<ManageNews />} />}
+          {isAdmin && (
+            <Route path="/admin/manage-reviews" element={<ManageReviews />} />
+          )}
+          {/* {isAdmin && <Route path="/admin/manage-news" element={<ManageNews />} />}
           {isAdmin && <Route path="/admin/manage-contact-me" element={<ManageContactMe />} />} */}
 
           <Route path="*" element={<Navigate to="/" />} />
