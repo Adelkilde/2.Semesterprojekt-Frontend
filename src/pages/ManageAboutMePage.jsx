ca67b1814c233ba1377d131ab13e899569b6d9fe
Oliver <115661717+OliKirk@users.noreply.github.com>
2023-12-11 16:31:22 +0100

    About Me CSS DONE!

--- src/pages/ManageAboutMePage.jsx
+++ src/pages/ManageAboutMePage.jsx
@@ -55,21 +55,34 @@ export default function ManageAboutMePage() {
 
   return (
     <div className="container mt-5">
-      <h1>Om Mig</h1>
-      <ul className="list-group">
-        {author.map((author) => (
-          <li key={author.author_id} className="list-group-item">
-            {author.image && <img src={author.image} alt="author Image" className="img-fluid" />}
-            <p className="mb-1">{author.name}</p>
-            <p className="mb-1">f. {author.birth_year}</p>
-            <p className="mb-1">{author.biography}</p>
-            <button onClick={() => handleEditAuthor(author)}>Rediger Info</button>
-          </li>
-        ))}
-      </ul>
-      {selectedAuthor && (
-        <AboutMeEditor saveAboutMe={handleSaveAuthor} onCancelEdit={handleCancelEdit} aboutMe={selectedAuthor} />
-      )}
-    </div>
+    <h1>Om Mig</h1>
+    <ul className="list-group">
+      {author.map((author) => (
+        <li key={author.author_id} className="list-group-item">
+          {author.image && <img src={author.image} alt="author Image" className="img-fluid" />}
+          <p className="mb-1">{author.name}</p>
+          <p className="mb-1">f. {author.birth_year}</p>
+          <p className="mb-1">{author.biography}</p>
+          <button onClick={() => handleEditAuthor(author)}>Rediger Info</button>
+        </li>
+      ))}
+    </ul>
+    {selectedAuthor && (
+      <div id="authorForm">
+        <label htmlFor="nameInput">Name:</label>
+        <input type="text" id="nameInput" />
+  
+        <label htmlFor="birthYearInput">Birth Year:</label>
+        <input type="text" id="birthYearInput" />
+  
+        <label htmlFor="biographyTextarea">Biography:</label>
+        <textarea id="biographyTextarea"></textarea>
+  
+        <button type="submit">Submit</button>
+      </div>
+    )}
+  </div>
+  
+  
   );
 }
