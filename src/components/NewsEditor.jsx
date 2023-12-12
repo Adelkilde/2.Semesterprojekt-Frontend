4947d74fe2abdbeadfeace93e22f6b182ade40e7
LauritsSchou <laurits.schou@gmail.com>
2023-12-12 17:25:49 +0100

    onCancelCreate now working

--- src/components/NewsEditor.jsx
+++ src/components/NewsEditor.jsx
@@ -1,66 +1,78 @@
 import { useEffect, useState } from "react";
 
-export default function NewsEditor({ saveNews, onCancelEdit, news }) {
-    const [formData, setFormData] = useState({
-        author_id: "1",
-        news_id: "",
-        headline: "",
-        content: "",
-    });
-    useEffect(() => {
-        if (news) {
-        setFormData({
-            headline: news.headline || "",
-            content: news.content || ""
-        });
-        }
-    }, [news]);
-    
-    const handleChange = (event) => {
-        setFormData({
-        ...formData,
-        [event.target.name]: event.target.value,
-        });
-    };
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        saveNews(formData);
-        resetForm();
-    };
-    
-    const resetForm = () => {
-        setFormData({
-            headline: "",
-            content: "",
-        });
-    };
-    
-    return (
-        <form onSubmit={handleSubmit}>
-        <label>
-            Overskrift
-            <input
-            type="text"
-            name="headline"
-            value={formData.headline}
-            onChange={handleChange}
-            />
-        </label>
-        <label>
-              Brødtekst
-            <textarea  name="content"  value={formData.content}  onChange={handleChange} />
-        </label>
-        <button type="submit">Gem</button>
-        {news && (
-            <button type="button" onClick={onCancelEdit}> Annuller ændringer </button>
-        )}
+export default function NewsEditor({
+  saveNews,
+  onCancelEdit,
+  news,
+  onCancelCreate,
+}) {
+  const [formData, setFormData] = useState({
+    author_id: "1",
+    news_id: "",
+    headline: "",
+    content: "",
+  });
+  useEffect(() => {
+    if (news) {
+      setFormData({
+        headline: news.headline || "",
+        content: news.content || "",
+      });
+    }
+  }, [news]);
 
-        {!news && (
-            <button type="button" onClick={onCancelEdit}>
-            Annuller oprettelse
-            </button>
-        )}
-        </form>
-    );
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    saveNews(formData);
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setFormData({
+      headline: "",
+      content: "",
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Overskrift
+        <input
+          type="text"
+          name="headline"
+          value={formData.headline}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Brødtekst
+        <textarea
+          name="content"
+          value={formData.content}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Gem</button>
+      {news && (
+        <button type="button" onClick={onCancelEdit}>
+          {" "}
+          Annuller ændringer{" "}
+        </button>
+      )}
+
+      {!news && (
+        <button type="button" onClick={onCancelCreate}>
+          Annuller oprettelse
+        </button>
+      )}
+    </form>
+  );
 }
