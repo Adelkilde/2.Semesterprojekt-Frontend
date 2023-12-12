366d364da30b7bde51afdd8ac99788d9e382d778
Oliver <115661717+OliKirk@users.noreply.github.com>
2023-12-12 13:11:06 +0100

    About Me DONE DONE FOR REAL

--- src/pages/ManageAboutMePage.jsx
+++ src/pages/ManageAboutMePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AboutMeEditor from "../components/AboutMeEditor";
 
 export default function ManageAboutMePage() {
   const [author, setAuthor] = useState([]);
@@ -21,36 +22,54 @@ export default function ManageAboutMePage() {
     setSelectedAuthor(author);
   };
 
+  const handleSaveAuthor = async (formData) => {
+    const url = `https://semesterprojekt2-deployment-with-azure.azurewebsites.net/author/${selectedAuthor.author_id}`;
+    const method = "PUT";
+
+    try {
+      const response = await fetch(url, {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log("Author updated successfully");
+        // Fetch the updated list of authors
+        fetchAuthor();
+        // Clear the selected author
+        setSelectedAuthor(null);
+      } else {
+        console.error("An error occurred:", response);
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+  const handleCancelEdit = () => {
+    setSelectedAuthor(null);
+  };
+
   return (
-    <div className="container mt-5">
-    <h1>Om Mig</h1>
-    <ul className="list-group">
-      {author.map((author) => (
-        <li key={author.author_id} className="list-group-item">
-          {author.image && <img src={author.image} alt="author Image" className="img-fluid" />}
-          <p className="mb-1">{author.name}</p>
-          <p className="mb-1">f. {author.birth_year}</p>
-          <p className="mb-1">{author.biography}</p>
-          <button onClick={() => handleEditAuthor(author)}>Rediger Info</button>
-        </li>
-      ))}
-    </ul>
-    {selectedAuthor && (
-      <div id="authorForm">
-        <label htmlFor="nameInput">Name:</label>
-        <input type="text" id="nameInput" />
-  
-        <label htmlFor="birthYearInput">Birth Year:</label>
-        <input type="text" id="birthYearInput" />
-  
-        <label htmlFor="biographyTextarea">Biography:</label>
-        <textarea id="biographyTextarea"></textarea>
-  
-        <button type="submit">Submit</button>
-      </div>
-    )}
-  </div>
-  
-  
+    <div id="authorForm" className="container mt-5">
+      <h1>Om Mig</h1>
+      <ul  className="list-group">
+        {author.map((author) => (
+          <li  key={author.author_id} className="list-group-item">
+            {author.image && <img src={author.image} alt="author Image" className="img-fluid" />}
+            <p className="mb-1">{author.name}</p>
+            <p className="mb-1">f. {author.birth_year}</p>
+            <p className="mb-1">{author.biography}</p>
+            <button onClick={() => handleEditAuthor(author)}>Rediger Info</button>
+          </li>
+        ))}
+      </ul>
+      {selectedAuthor && (
+        <AboutMeEditor saveAboutMe={handleSaveAuthor} onCancelEdit={handleCancelEdit} aboutMe={selectedAuthor} />
+      )}
+    </div>
   );
 }
